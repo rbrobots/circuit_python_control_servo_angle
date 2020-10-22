18ccce3674d1ff5334f8a5c6124744544fa4811c
Ranvir <rbrobots20@gmail.com>
2020-10-22 17:15:46 +0100

    successful merge of pca driver and wheel motor code

--- vegaMotor.py
+++ vegaMotor.py
@@ -1,8 +1,8 @@
 #import RPi.GPIO as GPIO
 from board import *
 import time, digitalio, pulseio, sys, busio
-#from adafruit_pca9685 import PCA9685
-#from adafruit_motor import servo
+from adafruit_pca9685 import PCA9685
+from adafruit_motor import servo
 
 class Motor():
 
@@ -21,8 +21,16 @@ class Motor():
         self.pwm_lw = pulseio.PWMOut(D12)
         self.pwm_rw = pulseio.PWMOut(D13)
         self.pwm_lw.duty_cycle = 0
+#        self.pwm_lw.frequency = 0
         self.pwm_rw.duty_cycle = 0
+#        self.pmw_rw.frequency = 0
         print("Wheel motor setup complete")
+        self.i2c=busio.I2C(SCL,SDA)
+        self.pca=PCA9685(self.i2c)
+        self.pca.frequency=50
+        self.servo0=servo.Servo(self.pca.channels[0], min_pulse=580, max_pulse=2480)
+        self.servo1=servo.Servo(self.pca.channels[1], min_pulse=580, max_pulse=2480)
+        print("Servo motor setup complete")
 
     def drive_forward(self):
         print("in drive forward")
@@ -74,13 +82,42 @@ class Motor():
         self.LWB.value = False
         self.RWF.value = False
         self.RWB.value = False
-        self.pwm_lw.frequency = 0
+#        self.pwm_lw.frequency = 0
         self.pwm_lw.duty_cycle = 0
-        self.pwm_rw.frequency = 0
+#        self.pwm_rw.frequency = 0
         self.pwm_rw.duty_cycle = 0
 
     def update_duty_cycle(self,c):
         self.duty_cycle=c
+
+    def servo_turn_to_angle(self,i,n):#i is channel and n is angle
+        if i==0 and n<=180 and n>=0:
+            for j in range(n):
+                self.servo0.angle=j
+                print(j)
+            time.sleep(2)
+        elif i==1 and n<=90 and n>=0:
+            for j in range(n):
+                self.servo1.angle=j
+                print(j)
+            time.sleep(1)
+        else:
+            print("Unknown input")
+
+    def testServo(self):
+        for i in range(180):
+            self.servo0.angle=i
+        time.sleep(1)
+        for i in range(180):
+            self.servo0.angle=180-i
+        time.sleep(1)
+        for i in range(90):
+            self.servo1.angle=i
+        time.sleep(1)
+        for i in range(90):
+            self.servo1.angle=90-i
+        time.sleep(1)
+        self.pca.deinit()
     def terminate_process(self):
         print("termination complete")
         exit(0)
