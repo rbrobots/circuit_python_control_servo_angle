dbf40f35d1b4d30dc1aabf544d0fdb6f97de97cc
rbrobots <69148588+rbrobots@users.noreply.github.com>
2020-11-19 11:14:42 +0000

    Delete Motor2.py

--- Motor2.py
+++
@@ -1,8 +0,0 @@
-from board import *
-import time, digitalio, pulseio, sys
-
-class Motor():
-    def __init__(self):
-        print("initialised")
-    def function1(self):
-        print("HELLO WORLD")
